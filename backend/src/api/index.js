7fa515c0cd121340a12000d780f2aa7197582a90
Thomas McEvoy <tmcevoy213@gmail.com>
2022-09-19 00:05:21 -0400

    make prod script

---
+++ backend/src/api/index.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+
+export default () => {
+  const router = Router()
+
+  router.get("/store/hello", (req, res) => {
+    res.json({
+      message: "Welcome to My Store!",
+    })
+  })
+
+  return router
+}
