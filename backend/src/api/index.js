8675b948f2442ff1c4f096c2f3214b1769b79794
Thomas McEvoy <tmcevoy213@gmail.com>
2022-09-19 12:39:59 -0400

    comment out custom endpoint

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,13 +1,13 @@
-import { Router } from "express"
+// import { Router } from "express"
 
-export default () => {
-  const router = Router()
+// export default () => {
+//   const router = Router()
 
-  router.get("/store/hello", (req, res) => {
-    res.json({
-      message: "Welcome to My Store!",
-    })
-  })
+//   router.get("/store/hello", (req, res) => {
+//     res.json({
+//       message: "Welcome to My Store!",
+//     })
+//   })
 
-  return router
-}
+//   return router
+// }
